6f584fabd53477162df0a765e5a562331ac0ceeb
maruchichi <123016635+maruchichi@users.noreply.github.com>
2023-01-25 17:11:40 -0300

    Add files via upload

---
+++ functionesdelcv.js
@@ -0,0 +1,9 @@
+document.getElementById('boton').onclick=function(){
+    console.log("Formacion");
+    document.getElementById("demo").innerHTML="Instituto superior de formacion tecnica n°177";
+}
+
+document.getElementById('boton2').onclick=function(){
+    console.log("referencias");
+    document.getElementById("demo2").innerHTML="Bioquimica:Brenda Azucena Leguizamon Cel:1131060395";
+}
